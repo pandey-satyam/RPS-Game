8c7aff73fafd05313aa34ece24d97c9374c5d518
pandey-satyam <68061957+pandey-satyam@users.noreply.github.com>
2021-06-20 04:27:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title> Satyam Pandey 09c0257f</title>
+</head>
+<body>
+<h2> Hey!! Excited? We have a game for you!!</h1>
+<h3> Rock Paper, Scissors!!Try Guessing Intelligently</h3>
+<h4 align="center"> <font color="black" size="25" face="Constantia"><u><marquee>Login to play</marquee></h4> </u></b></font>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Sorry!Attempt to go to
+<a href="game.php">game.php</a> without logging in will fail</p>
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Referenced Source Code for this Application as from Coursera</a>
+</p>
+</div>
+</body>
+</html>
